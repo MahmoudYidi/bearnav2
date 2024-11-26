7eb2e997d611396397fb3d19c70c89ff6cb891a6
MahmoudYidi <113612188+MahmoudYidi@users.noreply.github.com>
2024-11-26 13:52:36 +0000

    Update map_control.sh

--- map_control.sh
+++ map_control.sh
@@ -12,13 +12,13 @@ MAP_NAME=$2
 # Check the command and run the corresponding ROS 2 action
 case "$COMMAND" in
     start_map)
-        ros2 action send_goal /navigros2/mapmaker navigros2/action/MapMaker "{map_name: '$MAP_NAME', start: true}"
+        ros2 action send_goal /bearnav2/mapmaker bearnav2/action/MapMaker "{map_name: '$MAP_NAME', start: true}"
         ;;
     stop_map)
-        ros2 action send_goal /navigros2/mapmaker navigros2/action/MapMaker "{map_name: '$MAP_NAME', start: false}"
+        ros2 action send_goal /bearnav2/mapmaker bearnav2/action/MapMaker "{map_name: '$MAP_NAME', start: false}"
         ;;
     repeat_map)
-        ros2 action send_goal /navigros2/repeater navigros2/action/MapRepeater "{map_name: '$MAP_NAME'}"
+        ros2 action send_goal /bearnav2/repeater bearnav2/action/MapRepeater "{map_name: '$MAP_NAME'}"
         ;;
     *)
         echo "Unknown command: $COMMAND"
